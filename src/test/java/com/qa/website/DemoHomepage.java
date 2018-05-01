f57db6c1947c4c8a9cfa2853564e09d43cc9a0c0
cm17g13 <callummcgregor50@gmail.com>
2018-05-01 17:29:31 +0100

    Added report to tests

--- src/test/java/com/qa/website/DemoHomepage.java
+++ src/test/java/com/qa/website/DemoHomepage.java
@@ -11,12 +11,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class DemoHomepage {
 
 	static WebDriver driver = null;
+	private static ExtentReports report;
+	private ExtentTest dressTest;
+		
 	
 	@BeforeClass
 	public static void method() {
+		report = new ExtentReports("C:/Users/Admin/eclipse-workspace/SeleniumExamples/Reporting/target/BasicReport.html", true);
 		System.setProperty("webdriver.chrome.driver","C:/Development/chromedriver_win32/chromedriver.exe");
 		driver = new ChromeDriver();
 	}
@@ -28,35 +36,54 @@ public class DemoHomepage {
 	
 	@Test
 	public void DressSearch() {
+		dressTest = report.startTest("Verify application Title");
 		driver.get("http://automationpractice.com/index.php");
+		dressTest.log(LogStatus.INFO, "Browser started");
 		Homepage page = PageFactory.initElements(driver, Homepage.class);
 		sleep(1);
+		checkTitle(dressTest, "My Store");
 		page.setDriver(driver);
 		page.searchFor("Dress");
+		dressTest.log(LogStatus.INFO, "Dresses searched");
 		sleep(1);
 		SearchResultsPage dresses = PageFactory.initElements(driver, SearchResultsPage.class);
+		checkTitle(dressTest, "Search - My Store");
 		dresses.setDriver(driver);
 		dresses.addDress();
+		dressTest.log(LogStatus.INFO, "Dresses found");
 		sleep(1);
 		dresses.confirmDress();
 		CheckoutPage checkout = PageFactory.initElements(driver, CheckoutPage.class);
+		checkTitle(dressTest, "Order - My Store");
 		checkout.setDriver(driver);
 		checkout.changeQuantity(10);
+		dressTest.log(LogStatus.INFO, "Increased dress quantity");
 		sleep(1);
-		checkout.confirm();
-		
-		//WebElement checkElement = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span"));
-
-		//assertEquals("Selenium", checkElement.getText());
-		
+		checkout.confirm();		
 	}
+	
 
 	@After
-	public void tearDown() throws InterruptedException {
-
-		Thread.sleep(30000);
+	public void tearDown()  {
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		report.endTest(dressTest);
+		report.flush();
 		driver.close();
+	}
+	
+	public void checkTitle(ExtentTest test, String expectedTitle) {
 
+		if (driver.getTitle().equals(expectedTitle)) {
+			// report the test as a pass
+			test.log(LogStatus.PASS, "verify Title of the page");
+		} else {
+			test.log(LogStatus.FAIL, "verify Title of the page");
+		}
 	}
 	
 	public void sleep(int seconds) {
